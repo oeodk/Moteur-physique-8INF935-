97e6c383efbf1949f18b8f3f8ec2de9f8651a641
GONON Lucas <146363206+oeodk@users.noreply.github.com>
2024-08-29 15:00:51 -0400

    Vector3D added

---
+++ src/Vector3D.cpp
@@ -0,0 +1,149 @@
+#include "Vector3D.h"
+#include <cmath>
+
+Vector3D::Vector3D(float px, float py, float pz)
+    : x(px), y(py), z(pz)
+{
+
+}
+
+void Vector3D::normalize()
+{
+    set(*this / getNorm());
+}
+
+float Vector3D::getNorm() const
+{
+    return std::sqrt(squareNorm());
+}
+
+float Vector3D::squareNorm() const
+{
+    return (x * x + y * y + z * z);
+}
+
+void Vector3D::invert()
+{
+    x = -x;
+    y = -y;
+    z = -z;
+}
+
+
+void Vector3D::set(float vx, float vy, float vz)
+{
+    x = vx;
+    y = vy;
+    z = vz;
+}
+
+void Vector3D::operator*=(float value)
+{
+    x *= value;
+    y *= value;
+    z *= value;
+}
+
+void Vector3D::operator/=(float value)
+{
+    x /= value;
+    y /= value;
+    z /= value;
+}
+
+void Vector3D::operator+=(float value)
+{
+    x += value;
+    y += value;
+    z += value;
+}
+
+void Vector3D::operator-=(float value)
+{
+    x -= value;
+    y -= value;
+    z -= value;
+}
+
+void Vector3D::operator+=(const Vector3D& v)
+{
+    x += v.x;
+    y += v.y;
+    z += v.z;
+}
+
+void Vector3D::operator-=(const Vector3D& v)
+{
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+}
+
+Vector3D Vector3D::elementWiseProduct(const Vector3D& v1, const Vector3D v2)
+{
+    return Vector3D(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+
+Vector3D Vector3D::pow(const Vector3D& v, float exponent)
+{
+    return Vector3D(std::pow(v.x, exponent), std::pow(v.y, exponent), std::pow(v.z, exponent));
+}
+
+Vector3D  Vector3D::crossProduct(const Vector3D& v1, const Vector3D& v2)
+{
+    return Vector3D(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
+
+float  Vector3D::dotProduct(const Vector3D& v1, const Vector3D& v2)
+{
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+Vector3D operator+(const Vector3D& v1, const Vector3D& v2)
+{
+    return Vector3D(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+Vector3D operator-(const Vector3D& v1, const Vector3D& v2)
+{
+    return Vector3D(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+bool operator==(const Vector3D& v1, const Vector3D& v2)
+{
+    return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+}
+
+Vector3D operator*(const Vector3D& v1, float value)
+{
+    return Vector3D(v1.x * value, v1.y * value, v1.z * value);
+}
+
+Vector3D operator/(const Vector3D& v1, float value)
+{
+    return Vector3D(v1.x / value, v1.y / value, v1.z / value);
+}
+
+
+void testVector3D()
+{
+    Vector3D vec, vec2(1, 2, 3);
+    _ASSERT(vec.x == 0);
+    _ASSERT(vec.y == 0);
+    _ASSERT(vec.z == 0);
+
+    _ASSERT(vec2.x == 1);
+    _ASSERT(vec2.y == 2);
+    _ASSERT(vec2.z == 3);
+
+    _ASSERT(vec2 == Vector3D(1, 2, 3));
+
+    _ASSERT(vec2  + vec == vec2);
+    _ASSERT(vec2  + Vector3D(1,1,1) == Vector3D(2,3,4));
+    
+    _ASSERT(vec2  - vec == vec2);
+    _ASSERT(vec2  - Vector3D(1,1,1) == Vector3D(0,1,2));
+
+    _ASSERT(vec2 * 2 == Vector3D(2, 4, 6));
+    _ASSERT(vec2 / 2 == Vector3D(0.5f, 1, 1.5f));
+}
---
+++ src/Vector3D.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <glm/vec3.hpp>
+
+struct Vector3D
+{
+public :
+	float x, y, z;
+
+	Vector3D(float v = 0.f) : Vector3D(v, v, v){}
+	Vector3D(float px, float py, float pz);
+	Vector3D(const glm::vec3& v) : Vector3D(v.x, v.y, v.z) {}
+	Vector3D(const Vector3D&) = default;  // Default copy constructor
+
+	void normalize();
+	float getNorm() const;
+	float squareNorm() const;
+	void invert();
+	const float* data() const { return &x; }
+	void set(float vx, float vy, float vz);
+	void set(const Vector3D& v) { set(v.x, v.y, v.z); };
+
+
+	void operator*=(float value);
+	void operator/=(float value);
+	void operator+=(float value);
+	void operator-=(float value);
+	void operator+=(const Vector3D& v);
+	void operator-=(const Vector3D& v);
+
+	operator glm::vec3() { return glm::vec3(x, y, z); }
+
+	static Vector3D elementWiseProduct(const Vector3D& v1, const Vector3D v2);
+	static Vector3D pow(const Vector3D& v, float exponent);
+	static Vector3D crossProduct(const Vector3D& v1, const Vector3D& v2);
+	static float dotProduct(const Vector3D& v1, const Vector3D& v2);
+};
+
+Vector3D operator*(const Vector3D& v1, float value);
+Vector3D operator/(const Vector3D& v1, float value);
+Vector3D operator+(const Vector3D& v1, const Vector3D& v2);
+Vector3D operator-(const Vector3D& v1, const Vector3D& v2);
+bool operator==(const Vector3D& v1, const Vector3D& v2);
+
+void testVector3D();
