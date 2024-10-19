ec06190ac7ff0966415982e87faac4d28be256e9
Vollubane <loulouchu@laposte.net>
2024-10-19 18:00:40 -0400

    Réorganisation de la fonction physicEngine

--- src/PhysicsEngine.cpp
+++ src/PhysicsEngine.cpp
@@ -3,6 +3,10 @@
 void PhysicsEngine::updateParticles(float dt, const std::vector<class Particle*>& particles) {
     for (auto& p : particles) {
         //std::cout << std::to_string(p->getAcceleration().x) << ", " << std::to_string(p->getAcceleration().y) << ", " << std::to_string(p->getAcceleration().z) << "\n";
+        
+        p->computeForces();
+        p->integrate(dt, _integration_method);
+        p->clearAccum();
 
         for (auto& p2 : particles)
         {
@@ -11,8 +15,6 @@ void PhysicsEngine::updateParticles(float dt, const std::vector<class Particle*>
                 p->checkCollision(p2, dt);
             }
         }
-        p->computeForces();
-        p->integrate(dt, _integration_method);
-        p->clearAccum();
+        
     }
 }
